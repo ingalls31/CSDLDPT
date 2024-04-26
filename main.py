1f3e672947260f64b8e9b2f8ed0aaf57ed040924
baovietcat <vietcr999@gmail.com>
2024-04-26 16:15:53 +0700

    new update 1.2 main.py

--- main.py
+++ main.py
@@ -4,41 +4,25 @@ from kdtree import CustomKDTree
 import pickle
 import os
 
-# init = Init("audio")
-# init.init()
-
-# with open("sounds.obj", "rb") as s:
-#     sounds = pickle.load(s)
-
-# tree = CustomKDTree(sounds=sounds)
-
-# extract = Extract(normalize_path="normalize.obj", sound_path="test_audio.mp3")
-# vector = extract.features()
-
-# query = Sound(features=vector, path="test_audio.mp3")
-# result = tree.query_knn(sound_query=query, k=5)
-# for path, dis in result:
-#     print(f"{path} --> {dis}")
-
 while True:
-    query = input()
+    query = input(">")
     if query == "exit": break
     elif query == "init":
         init = Init("audio")
         init.init()
     elif query.startswith("find "):
-        file_path = query[5:]
-        if not os.path.isfile("query/" + file_path): print(f"File {file_path} was not found.")
+        file_path = "query/" + query[5:]
+        if not os.path.isfile(file_path): print(f"File {file_path} was not found.")
         else:
             with open("sounds.obj", "rb") as s:
                 sounds = pickle.load(s)
 
             tree = CustomKDTree(sounds=sounds)
 
-            extract = Extract(normalize_path="normalize.obj", sound_path="test_audio.mp3")
+            extract = Extract(normalize_path="normalize.obj", sound_path=file_path)
             vector = extract.features()
 
-            query = Sound(features=vector, path="test_audio.mp3")
+            query = Sound(features=vector, path=file_path)
             result = tree.query_knn(sound_query=query, k=5)
             for path, dis in result:
                 print(f"{path} --> {dis}")
